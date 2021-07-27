6390f22fa674064ae0daded995f7a442c7be53e8
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-07-27 18:41:02 -0500

    Add comparisons

--- anydep/params.py
+++ anydep/params.py
@@ -3,5 +3,5 @@ from typing import Optional
 from anydep.models import Dependant, DependencyProviderType, DependencyType, Scope
 
 
-def Depends(call: Optional[DependencyProviderType] = None, *, scope: Optional[Scope] = None) -> DependencyType:
+def Depends(call: Optional[DependencyProviderType[DependencyType]] = None, *, scope: Optional[Scope] = None) -> DependencyType:
     return Dependant(call=call, scope=scope)  # type: ignore
---
+++ comparisons/anydep_asyncpg.py
@@ -0,0 +1,44 @@
+import asyncio
+from time import time
+from typing import AsyncGenerator
+
+import asyncpg
+
+from anydep.container import Container
+from anydep.params import Depends
+
+
+async def db_conn_pool() -> AsyncGenerator[asyncpg.Connection, None]:
+    async with asyncpg.create_pool(user="postgres", password="mysecretpassword") as pool:
+        yield pool
+
+
+async def db_conn(pool: asyncpg.Pool = Depends(db_conn_pool)) -> AsyncGenerator[asyncpg.Connection, None]:
+    async with pool.acquire() as conn:
+        yield conn
+
+
+async def endpoint(conn: asyncpg.Connection = Depends(db_conn)) -> int:
+    return await conn.fetchval("select 2 ^ 8")
+
+
+async def test(container: Container):
+    r = await container.resolve(endpoint)
+    assert r == 256
+
+
+async def main():
+    container = Container()
+    t = []
+    for _ in range(100):  # 100 incoming requests
+        async with container.enter_scope("request"):
+            start = time()
+            await test(container)
+        t.append(time() - start)
+    average = sum(t) / len(t)
+    worst = max(t)
+    worst_idx = t.index(worst)
+    print(f"Average: {average:0.5f}\nWorst: {worst:0.5f}@{worst_idx}")
+
+
+asyncio.run(main())
