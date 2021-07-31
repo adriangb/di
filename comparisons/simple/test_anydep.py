5c0e595863e23da611c2b1c25a72e634ea1dc128
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-07-31 11:45:09 -0500

    Add simple comparison to python-dependency-injector

---
+++ comparisons/simple/test_anydep.py
@@ -0,0 +1,32 @@
+import time
+
+import anyio
+
+from anydep.container import Container
+from anydep.params import Depends
+
+
+async def coroutine1():
+    await anyio.sleep(1)
+    return 1
+
+
+async def coroutine2():
+    await anyio.sleep(1)
+    return 2
+
+
+async def collector(v1: int = Depends(coroutine1), v2: int = Depends(coroutine2)):
+    return v1 + v2
+
+
+async def main():
+    container = Container()
+    start = time.time()
+    async with container.enter_global_scope("app"):
+        await container.execute(container.get_dependant(collector))
+    print(time.time() - start)
+
+
+if __name__ == "__main__":
+    anyio.run(main)
