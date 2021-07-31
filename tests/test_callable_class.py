f119f3689f26719cf250700d5a1e0a28b0027015
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-07-31 14:39:16 -0500

    Add test for loading non-wired class from config:

--- tests/test_callable_class.py
+++ tests/test_callable_class.py
@@ -4,22 +4,39 @@ from anydep.container import Container
 from anydep.params import CallableClass
 
 
-class MyClass:
+class Config:
     def __init__(self) -> None:
-        self.value = 2
+        self.value: int = 1
+
+
+class MyClass:
+    def __init__(self, value: int = 2) -> None:
+        self.value = value
 
     async def __call__(self):
-        return self.value ** 2
+        return self.value
 
 
 class MyChildClass(MyClass):
-    def __init__(self) -> None:
-        self.value = 3
+    def __init__(self, value: int = 2) -> None:
+        self.value = value ** 2
+
+
+def myclass_from_config(config: Config) -> MyClass:
+    return MyClass(value=config.value)
 
 
 @pytest.mark.anyio
-async def test_callable_class():
+async def test_loading_from_config():
     container = Container()
     async with container.enter_global_scope("app"):
-        assert 4 == await container.execute(CallableClass(MyClass))
-        assert 9 == await container.execute(CallableClass(MyChildClass))
+        container.bind(MyClass, myclass_from_config)
+        assert 1 == await container.execute(CallableClass(MyClass))
+
+
+@pytest.mark.anyio
+async def test_inheritence():
+    container = Container()
+    async with container.enter_global_scope("app"):
+        assert 2 == await container.execute(CallableClass(MyClass))
+        assert 4 == await container.execute(CallableClass(MyChildClass))
