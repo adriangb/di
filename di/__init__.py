cf81eed7519af49d48a17b1c28d3b262cd62d980
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-09-25 12:50:27 -0500

    Add version check to PRs

--- di/__init__.py
+++ di/__init__.py
@@ -1,3 +1,8 @@
+import importlib.metadata as _importlib_metadata
+
+__version__ = _importlib_metadata.version(__name__)
+
+
 from di.container import Container
 from di.dependency import Dependant, DependantProtocol
 from di.params import Depends
