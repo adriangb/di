6d38028de8f938fe25e88c96daa0a2e0035bcf64
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-10-12 10:20:12 -0500

    feat!: split up eDefaultExecutor and sprovide a SimpleSyncExecutor

--- di/executors.py
+++ di/executors.py
@@ -11,7 +11,45 @@ from di.types.executor import AsyncExecutor, SyncExecutor, Task, Values
 ResultType = typing.TypeVar("ResultType")
 
 
-class DefaultExecutor(AsyncExecutor, SyncExecutor):
+class SimpleSyncExecutor(SyncExecutor):
+    def execute_sync(
+        self,
+        tasks: typing.List[typing.List[Task]],
+        get_result: typing.Callable[[], ResultType],
+        values: Values,
+    ) -> ResultType:
+        for task_group in tasks:
+            for task in task_group:
+                result = task(values)
+                if inspect.isawaitable(result):
+                    raise TypeError("Cannot execute async dependencies in execute_sync")
+        return get_result()
+
+
+class ConcurrentAsyncExecutor(AsyncExecutor):
+    async def execute_async(
+        self,
+        tasks: typing.List[typing.List[Task]],
+        get_result: typing.Callable[[], ResultType],
+        values: Values,
+    ) -> ResultType:
+        # note: there are 2 task group concepts in this function that should not be confused
+        # to di, tasks groups are a set of Task's that can be executed in parallel
+        # to anyio, a TaskGroup is a primitive equivalent to a Trio nursery
+        tg: typing.Optional[anyio.abc.TaskGroup] = None
+        for task_group in tasks:
+            if len(task_group) > 1:
+                if tg is None:
+                    tg = anyio.create_task_group()
+                async with tg:
+                    for task in task_group:
+                        tg.start_soon(gurantee_awaitable(task), values)  # type: ignore
+            else:
+                await gurantee_awaitable(next(iter(task_group)))(values)
+        return get_result()
+
+
+class ConcurrentSyncExecutor(SyncExecutor):
     def __init__(self) -> None:
         self._threadpool = concurrent.futures.ThreadPoolExecutor()
 
@@ -44,23 +82,6 @@ class DefaultExecutor(AsyncExecutor, SyncExecutor):
                     raise TypeError("Cannot execute async dependencies in execute_sync")
         return get_result()
 
-    async def execute_async(
-        self,
-        tasks: typing.List[typing.List[Task]],
-        get_result: typing.Callable[[], ResultType],
-        values: Values,
-    ) -> ResultType:
-        # note: there are 2 task group concepts in this function that should not be confused
-        # to di, tasks groups are a set of Task's that can be executed in parallel
-        # to anyio, a TaskGroup is a primitive equivalent to a Trio nursery
-        tg: typing.Optional[anyio.abc.TaskGroup] = None
-        for task_group in tasks:
-            if len(task_group) > 1:
-                if tg is None:
-                    tg = anyio.create_task_group()
-                async with tg:
-                    for task in task_group:
-                        tg.start_soon(gurantee_awaitable(task), values)  # type: ignore
-            else:
-                await gurantee_awaitable(next(iter(task_group)))(values)
-        return get_result()
+
+class DefaultExecutor(ConcurrentAsyncExecutor, ConcurrentSyncExecutor):
+    ...
--- tests/test_executors.py
+++ tests/test_executors.py
@@ -1,20 +1,39 @@
+import typing
 from typing import Awaitable, Callable, List
 
 import pytest
 
-from di.executors import DefaultExecutor
-from di.types.executor import Values
+from di.executors import ConcurrentSyncExecutor, DefaultExecutor, SimpleSyncExecutor
+from di.types.executor import SyncExecutor, Values
 
 
 async def task(values: Values) -> None:
     ...
 
 
-@pytest.mark.anyio
 @pytest.mark.parametrize("tasks", [[[task]], [[task, task]]])
-async def test_async_dep_execute_sync(tasks: List[List[Callable[[], Awaitable[None]]]]):
-    exc = DefaultExecutor()
+@pytest.mark.parametrize(
+    "exc_cls", [DefaultExecutor, SimpleSyncExecutor, ConcurrentSyncExecutor]
+)
+def test_executing_async_dependencies_in_sync_executor(
+    tasks: List[List[Callable[[], Awaitable[None]]]], exc_cls: typing.Type[SyncExecutor]
+):
+    exc = exc_cls()
     with pytest.raises(
         TypeError, match="Cannot execute async dependencies in execute_sync"
     ):
         exc.execute_sync(tasks, lambda: None, {})  # type: ignore
+
+
+def test_simple_sync_executor():
+    class Dep:
+        called = False
+
+        def __call__(self, values: Values) -> None:
+            self.called = True
+
+    tasks = [[Dep()], [Dep(), Dep()]]
+
+    exc = SimpleSyncExecutor()
+
+    exc.execute_sync(tasks, lambda: None, {})
