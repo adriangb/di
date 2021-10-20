d5285b32113d412cd06aa3cb6fb315c938938e64
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-10-20 12:55:48 -0500

    perf: better benchmarks

--- benchmarks/solve.py
+++ benchmarks/solve.py
@@ -8,35 +8,77 @@ import cProfile
 import pstats
 import timeit
 
+import anyio
+
 from benchmarks.utils import generate_dag
 from di import Container, Dependant, Depends
-from di.executors import SimpleSyncExecutor
-
-root = generate_dag(Depends, 1, 1, 1, sync=True)
+from di.executors import ConcurrentAsyncExecutor, SimpleSyncExecutor
 
 
-def endpoint(dag: None = Depends(root)):
-    return ...
-
-
-container = Container(executor=SimpleSyncExecutor())
-solved = container.solve(Dependant(endpoint))
-
-
-def bench():
-    container.execute_sync(solved)
-
-
-def main():
+async def async_concurrent():
+    container = Container(executor=ConcurrentAsyncExecutor())
+    solved = container.solve(
+        Dependant(generate_dag(Depends, 4, 3, 3, sync=False, sleep=(0, 0.01)))
+    )
     profiler = cProfile.Profile()
     profiler.enable()
-    bench()
+    await container.execute_async(solved)
     profiler.disable()
     stats = pstats.Stats(profiler)
-    stats.dump_stats(filename="bench.prof")
-    print("Dumped cProfile stats to bench.prof")
-    print(timeit.timeit("bench()", globals=globals(), number=10000))
+    filename = "async_concurrent.prof"
+    stats.dump_stats(filename=filename)
+    print(f"Dumped cProfile stats to {filename}")
+    start = timeit.default_timer()
+    iters = 100
+    for _ in range(iters):
+        await container.execute_async(solved)
+    elapsed = timeit.default_timer() - start
+    print(f"{elapsed/iters:.2e} sec/iter")
+
+
+def sync_sequential_large():
+    container = Container(executor=SimpleSyncExecutor())
+    solved = container.solve(
+        Dependant(generate_dag(Depends, 4, 3, 3, sync=True, sleep=(0, 1e-5)))
+    )
+    profiler = cProfile.Profile()
+    profiler.enable()
+    container.execute_sync(solved)
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    filename = "sync_sequential_large.prof"
+    stats.dump_stats(filename=filename)
+    print(f"Dumped cProfile stats to {filename}")
+    start = timeit.default_timer()
+    iters = 100
+    for _ in range(iters):
+        container.execute_sync(solved)
+    elapsed = timeit.default_timer() - start
+    print(f"{elapsed/iters:.2e} sec/iter")
+
+
+def sync_sequential_small():
+    container = Container(executor=SimpleSyncExecutor())
+    solved = container.solve(
+        Dependant(generate_dag(Depends, 1, 1, 1, sync=True, sleep=(0, 1e-5)))
+    )
+    profiler = cProfile.Profile()
+    profiler.enable()
+    container.execute_sync(solved)
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    filename = "sync_sequential_small.prof"
+    stats.dump_stats(filename=filename)
+    print(f"Dumped cProfile stats to {filename}")
+    start = timeit.default_timer()
+    iters = 100
+    for _ in range(iters):
+        container.execute_sync(solved)
+    elapsed = timeit.default_timer() - start
+    print(f"{elapsed/iters:.2e} sec/iter")
 
 
 if __name__ == "__main__":
-    main()
+    anyio.run(async_concurrent)
+    sync_sequential_large()
+    sync_sequential_small()
--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -1,5 +1,8 @@
+import time  # noqa
 from random import Random
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, Tuple
+
+import anyio  # noqa
 
 random = Random(0)
 
@@ -11,10 +14,17 @@ def generate_dag(
     dependencies_per_node: int,
     *,
     sync: bool = False,
+    sleep: Tuple[float, float] = (0, 0),
 ) -> Callable[..., None]:
     """Build a complex DAG of async dependencies"""
+    sleep_func = time.sleep if sync else anyio.sleep
 
-    template = "def func_{}({}): ..." if sync else "async def func_{}({}): ..."
+    template = (
+        "def func_{}({}): sleep({})"
+        if sync
+        else "async def func_{}({}): await sleep({})"
+    )
+    globals = {"Depends": depends, "sleep": sleep_func}
 
     funcs: Dict[str, Callable[..., Any]] = {}
     for level in range(levels):
@@ -26,13 +36,14 @@ def generate_dag(
             params = ", ".join(
                 [f"dep_{dep_name}: None = Depends({dep_name})" for dep_name in deps]
             )
-            func_def = template.format(name, params)
-            exec(func_def, {"Depends": depends}, funcs)
+            sleep_time = random.uniform(sleep[0], sleep[1])
+            func_def = template.format(name, params, sleep_time)
+            exec(func_def, globals, funcs)
     name = "final"
     deps = list(funcs.keys())
     params = ", ".join(
         [f"dep_{dep_name}: None = Depends({dep_name})" for dep_name in deps]
     )
-    func_def = template.format(name, params)
-    exec(func_def, {"Depends": depends}, funcs)
+    func_def = template.format(name, params, 0)
+    exec(func_def, globals, funcs)
     return funcs["func_final"]
