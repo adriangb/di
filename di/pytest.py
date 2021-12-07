a264827810f2423a86ba5e53ae686715f8bb344d
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-12-07 00:46:17 -0600

    remove unecessary markers & simplify impl

--- di/pytest.py
+++ di/pytest.py
@@ -1,4 +1,4 @@
-from typing import AsyncGenerator, Callable, Coroutine, Optional, Union, overload
+from typing import AsyncGenerator, Callable, Coroutine, Union
 
 try:
     import pytest
@@ -10,88 +10,7 @@ except ImportError:
 
 from di import Container, Dependant
 from di._utils.inspect import is_async_gen_callable, is_coroutine_callable
-from di.api.providers import (
-    AsyncGeneratorProvider,
-    CallableProvider,
-    CoroutineProvider,
-    DependencyProviderType,
-    DependencyType,
-    GeneratorProvider,
-)
-from di.api.scopes import Scope
 from di.executors import SimpleAsyncExecutor
-
-
-@overload
-def Depends(
-    call: Optional[AsyncGeneratorProvider[DependencyType]] = None,
-    *,
-    scope: Scope = "function",
-    share: bool = True,
-    wire: bool = True,
-    autowire: bool = True,
-    sync_to_thread: bool = False,
-) -> DependencyType:
-    ...
-
-
-@overload
-def Depends(
-    call: Optional[CoroutineProvider[DependencyType]] = None,
-    *,
-    scope: Scope = "function",
-    share: bool = True,
-    wire: bool = True,
-    autowire: bool = True,
-    sync_to_thread: bool = False,
-) -> DependencyType:
-    ...
-
-
-@overload
-def Depends(
-    call: Optional[GeneratorProvider[DependencyType]] = None,
-    *,
-    scope: Scope = "function",
-    share: bool = True,
-    wire: bool = True,
-    autowire: bool = True,
-    sync_to_thread: bool = False,
-) -> DependencyType:
-    ...
-
-
-@overload
-def Depends(
-    call: Optional[CallableProvider[DependencyType]] = None,
-    *,
-    scope: Scope = "function",
-    share: bool = True,
-    wire: bool = True,
-    autowire: bool = True,
-    sync_to_thread: bool = False,
-) -> DependencyType:
-    ...
-
-
-def Depends(
-    call: Optional[DependencyProviderType[DependencyType]] = None,
-    *,
-    scope: Scope = "function",
-    share: bool = True,
-    wire: bool = True,
-    autowire: bool = True,
-    sync_to_thread: bool = False,
-) -> DependencyType:
-    return Dependant(  # type: ignore[return-value]
-        call=call,
-        scope=scope,
-        share=share,
-        wire=wire,
-        autowire=autowire,
-        sync_to_thread=sync_to_thread,
-    )
-
 
 _container = Container(execution_scope="function", executor=SimpleAsyncExecutor())
 _container.bind(
@@ -131,13 +50,7 @@ async def enter_function_scope(container: Container) -> AsyncGenerator[None, Non
 def inject(
     func: Union[Callable[..., None], Callable[..., Coroutine[None, None, None]]]
 ) -> Union[Callable[..., None], Callable[..., Coroutine[None, None, None]]]:
-    dep = Dependant(
-        func,
-        share=False,
-        scope="function",
-        wire=True,
-        autowire=True,
-    )
+    dep = Dependant(func, scope="function")
 
     # TODO: manipulate signature to preserve compatibility w/ Pytest fixtures?
     # This would entail removing parameters that di will inject from the signature
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -2,20 +2,21 @@ from typing import AsyncGenerator, List
 
 import pytest
 
-from di.pytest import Depends, inject
+from di import Depends
+from di.pytest import inject
 
 
 class Log(List[str]):
     pass
 
 
-def dep(log: Log = Depends(Log)) -> int:
+def dep(log: Log) -> int:
     log.append("dep")
     return 1
 
 
 async def generator_dep(
-    inner_val: int = Depends(dep), log: Log = Depends(Log)
+    log: Log, inner_val: int = Depends(dep, scope="function")
 ) -> AsyncGenerator[int, None]:
     log.append("generator_dep setup")
     yield inner_val + 1
@@ -24,6 +25,8 @@ async def generator_dep(
 
 @pytest.mark.anyio
 @inject
-async def test_inject(v: int = Depends(generator_dep), log: Log = Depends(Log)) -> None:
+async def test_inject(
+    log: Log, v: int = Depends(generator_dep, scope="function")
+) -> None:
     assert v == 2
     assert log == ["dep", "generator_dep setup"]  # teardown hasn't run yet
