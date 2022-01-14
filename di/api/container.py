adriangb/di

commit e87ba2e037a494fc05d1bf1a1707187c90c9524b

feat!: allow passing a provider directly to bind()
Closes #15

di/api/container.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Collection, ContextManager, Mapping, Optional, TypeVar
+from typing import Any, Collection, ContextManager, Mapping, Optional, TypeVar, Union
 
 if sys.version_info < (3, 8):
     from typing_extensions import Protocol
@@ -25,7 +25,9 @@ class ContainerProtocol(Protocol):
 
     def bind(
         self,
-        provider: DependantBase[DependencyType],
+        provider: Union[
+            DependantBase[DependencyType], DependencyProviderType[DependencyType]
+        ],
         dependency: DependencyProviderType[DependencyType],
     ) -> ContextManager[None]:
         """Bind a new dependency provider for a given dependency.

di/container.py
@@ -36,6 +36,7 @@ from di.api.executor import AsyncExecutor, SyncExecutor
 from di.api.providers import DependencyProvider, DependencyProviderType
 from di.api.scopes import Scope
 from di.api.solved import SolvedDependant
+from di.dependant import Dependant
 from di.exceptions import SolvingError, WiringError
 from di.executors import DefaultExecutor
 
@@ -84,10 +85,12 @@ class _ContainerCommon:
 
     def bind(
         self,
-        provider: DependantBase[DependencyType],
+        provider: Union[
+            DependantBase[DependencyType], DependencyProviderType[DependencyType]
+        ],
         dependency: DependencyProviderType[DependencyType],
     ) -> ContextManager[None]:
-        """Bind a new dependency provider for a given dependency.
+        """Replace a dependency provider with a new one.
 
         This can be used as a function (for a permanent bind, cleared when `scope` is exited)
         or as a context manager (the bind will be cleared when the context manager exits).
@@ -96,6 +99,9 @@ class _ContainerCommon:
         the scope or any other data from the dependency they are replacing.
         """
         previous_provider = self.binds.get(dependency, None)
+
+        if not isinstance(provider, DependantBase):
+            provider = Dependant(provider)
 
         self._binds[dependency] = provider
 

docs/src/bind_as_a_dep.py
@@ -1,6 +1,5 @@
 import sys
 from dataclasses import dataclass
-from typing import List
 
 if sys.version_info < (3, 8):
     from typing_extensions import Protocol
@@ -27,18 +26,18 @@ class DBConfig:
 class Postgres(DBProtocol):
     def __init__(self, config: DBConfig) -> None:
         self.host = config.host
-        self.log: List[str] = []
 
     async def execute(self, sql: str) -> None:
-        self.log.append(sql)
+        print(sql)
 
 
 async def framework() -> None:
     container = Container()
-    container.bind(Dependant(Postgres, scope="app"), DBProtocol)  # type: ignore
+    container.bind(Postgres, DBProtocol)
     solved = container.solve(Dependant(controller))
-    async with container.enter_scope("app"):
-        await container.execute_async(solved)
-        db = await container.execute_async(container.solve(Dependant(DBProtocol)))
-        assert isinstance(db, Postgres)
-        assert db.log == ["SELECT *"]
+    # this next line would fail without the bind
+    await container.execute_async(solved)
+    # and we can double check that the bind worked
+    # by requesting the instance directly
+    db = await container.execute_async(container.solve(Dependant(DBProtocol)))
+    assert isinstance(db, Postgres)

tests/test_binding.py
@@ -20,10 +20,10 @@ def test_bind():
         with container.enter_scope("request"):
             request = Request(1)  # build a request
             # bind the request
-            with container.bind(Dependant(lambda: request), Request):
+            with container.bind(lambda: request, Request):
                 r = container.execute_sync(container.solve(Dependant(endpoint)))
                 assert r == 1  # bound value
-                with container.bind(Dependant(lambda: Request(2)), Request):
+                with container.bind(lambda: Request(2), Request):
                     r = container.execute_sync(container.solve(Dependant(endpoint)))
                     assert r == 2
                 r = container.execute_sync(container.solve(Dependant(endpoint)))
@@ -61,7 +61,7 @@ def test_bind_transitive_dependency_results_skips_subdpendencies():
         def not_error() -> None:
             ...
 
-        with container.bind(Dependant(not_error), transitive):
+        with container.bind(not_error, transitive):
             container.execute_sync(container.solve(Dependant(dep)))
         # and this reverts when the bind exits
         with pytest.raises(ValueError):
@@ -86,7 +86,7 @@ def test_bind_with_dependencies():
     container = Container()
     with container.enter_scope("something"):
         assert (container.execute_sync(container.solve(Dependant(return_four)))) == 4
-        with container.bind(Dependant(return_three), return_two):
+        with container.bind(return_three, return_two):
             assert (
                 container.execute_sync(container.solve(Dependant(return_four)))
             ) == 5

tests/test_exceptions.py
@@ -49,7 +49,7 @@ def test_dependency_can_catch_exception_single_sync() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     container.execute_sync(container.solve(Dependant(collector)))
     assert rec.caught == {"dep1": True}
 
@@ -61,7 +61,7 @@ async def test_dependency_can_catch_exception_single_async() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     assert rec.caught == {"async_dep1": True}
 
@@ -72,7 +72,7 @@ def test_dependency_can_catch_exception_concurrent_sync() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     container.execute_sync(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -89,7 +89,7 @@ async def test_dependency_can_catch_exception_concurrent_async() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -104,7 +104,7 @@ async def test_dependency_can_catch_exception_concurrent_mixed() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -150,7 +150,7 @@ def test_dependency_can_catch_exception_single_sync_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     try:
         container.execute_sync(container.solve(Dependant(collector)))
     except MyException:
@@ -169,7 +169,7 @@ async def test_dependency_can_catch_exception_single_async_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:
@@ -189,7 +189,7 @@ def test_dependency_can_catch_exception_concurrent_sync_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     try:
         container.execute_sync(container.solve(Dependant(collector)))
     except MyException:
@@ -210,7 +210,7 @@ async def test_dependency_can_catch_exception_concurrent_async_reraise() -> None
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:
@@ -231,7 +231,7 @@ async def test_dependency_can_catch_exception_concurrent_mixed_reraise() -> None
 
     container = Container()
     rec = Recorder()
-    container.bind(Dependant(lambda: rec), Recorder)
+    container.bind(lambda: rec, Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:

tests/test_execute.py
@@ -272,7 +272,7 @@ async def test_concurrency_async(dep1: Any, dep2: Any):
     container = Container()
 
     counter = Counter()
-    container.bind(Dependant(lambda: counter), Counter)
+    container.bind(lambda: counter, Counter)
 
     async def collector(
         a: None = Depends(dep1, share=False, sync_to_thread=True),

tests/test_solving.py
@@ -80,7 +80,7 @@ def test_valid_non_callable_annotation(
     """No type annotations are required if default values are provided"""
 
     container = Container()
-    container.bind(Dependant(lambda: 1), annotation)
+    container.bind(lambda: 1, annotation)
     res = container.execute_sync(container.solve(Dependant(function)))
     assert res == 1
 
@@ -205,9 +205,9 @@ def test_wiring_from_binds() -> None:
             super().__init__(1)
 
     container = Container()
-    # container.bind(Dependant(CanBeWired), CannotBeWired)
+    # container.bind(CanBeWired, CannotBeWired)
     with pytest.raises(WiringError):
         container.solve(Dependant(CannotBeWired))
-    container.bind(Dependant(CanBeWired), CannotBeWired)  # type: ignore[arg-type]
+    container.bind(CanBeWired, CannotBeWired)
     c = container.execute_sync(container.solve(Dependant(CannotBeWired)))
     assert isinstance(c, CanBeWired)

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "di"
-version = "0.36.3"
+version = "0.37.0"
 description = "Autowiring dependency injection"
 authors = ["Adrian Garcia Badaracco <adrian@adriangb.com>"]
 readme = "README.md"

commit df79a9450601b86c2caea9baa56d220b0d3793b0

BREAKING_CHANGE: only allow dependants to be bound
This reverts e87ba2e037a494fc05d1bf1a1707187c90c9524b while still fixing #15 by removing the type annotations

The changes in e87ba2e037a494fc05d1bf1a1707187c90c9524b caused issues with scopes: since a default Dependant was being created, it's scope would be the default scope for di.dependant.Dependant, which might not match the scopes the end system used, causing confusing errors for users regarding scopes

di/api/container.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Collection, ContextManager, Mapping, Optional, TypeVar, Union
+from typing import Any, Collection, ContextManager, Mapping, Optional, TypeVar
 
 if sys.version_info < (3, 8):
     from typing_extensions import Protocol
@@ -25,10 +25,8 @@ class ContainerProtocol(Protocol):
 
     def bind(
         self,
-        provider: Union[
-            DependantBase[DependencyType], DependencyProviderType[DependencyType]
-        ],
-        dependency: DependencyProviderType[DependencyType],
+        provider: DependantBase[Any],
+        dependency: DependencyProviderType[Any],
     ) -> ContextManager[None]:
         """Bind a new dependency provider for a given dependency.
 

di/container.py
@@ -36,7 +36,6 @@ from di.api.executor import AsyncExecutor, SyncExecutor
 from di.api.providers import DependencyProvider, DependencyProviderType
 from di.api.scopes import Scope
 from di.api.solved import SolvedDependant
-from di.dependant import Dependant
 from di.exceptions import SolvingError, WiringError
 from di.executors import DefaultExecutor
 
@@ -72,10 +71,6 @@ class _ContainerCommon:
         self._binds = binds or {}
 
     @property
-    def binds(self) -> Mapping[DependencyProvider, DependantBase[Any]]:
-        return self._binds
-
-    @property
     def scopes(self) -> Collection[Scope]:
         return self._state.stacks.keys()
 
@@ -85,10 +80,8 @@ class _ContainerCommon:
 
     def bind(
         self,
-        provider: Union[
-            DependantBase[DependencyType], DependencyProviderType[DependencyType]
-        ],
-        dependency: DependencyProviderType[DependencyType],
+        provider: DependantBase[Any],
+        dependency: DependencyProviderType[Any],
     ) -> ContextManager[None]:
         """Replace a dependency provider with a new one.
 
@@ -98,10 +91,7 @@ class _ContainerCommon:
         Binds are only identified by the identity of the callable and do not take into account
         the scope or any other data from the dependency they are replacing.
         """
-        previous_provider = self.binds.get(dependency, None)
-
-        if not isinstance(provider, DependantBase):
-            provider = Dependant(provider)
+        previous_provider = self._binds.get(dependency, None)
 
         self._binds[dependency] = provider
 

docs/src/bind_as_a_dep.py
@@ -33,7 +33,7 @@ class Postgres(DBProtocol):
 
 async def framework() -> None:
     container = Container()
-    container.bind(Postgres, DBProtocol)
+    container.bind(Dependant(Postgres), DBProtocol)
     solved = container.solve(Dependant(controller))
     # this next line would fail without the bind
     await container.execute_async(solved)

tests/test_binding.py
@@ -20,10 +20,10 @@ def test_bind():
         with container.enter_scope("request"):
             request = Request(1)  # build a request
             # bind the request
-            with container.bind(lambda: request, Request):
+            with container.bind(Dependant(lambda: request), Request):
                 r = container.execute_sync(container.solve(Dependant(endpoint)))
                 assert r == 1  # bound value
-                with container.bind(lambda: Request(2), Request):
+                with container.bind(Dependant(lambda: Request(2)), Request):
                     r = container.execute_sync(container.solve(Dependant(endpoint)))
                     assert r == 2
                 r = container.execute_sync(container.solve(Dependant(endpoint)))
@@ -61,7 +61,7 @@ def test_bind_transitive_dependency_results_skips_subdpendencies():
         def not_error() -> None:
             ...
 
-        with container.bind(not_error, transitive):
+        with container.bind(Dependant(not_error), transitive):
             container.execute_sync(container.solve(Dependant(dep)))
         # and this reverts when the bind exits
         with pytest.raises(ValueError):
@@ -86,7 +86,7 @@ def test_bind_with_dependencies():
     container = Container()
     with container.enter_scope("something"):
         assert (container.execute_sync(container.solve(Dependant(return_four)))) == 4
-        with container.bind(return_three, return_two):
+        with container.bind(Dependant(return_three), return_two):
             assert (
                 container.execute_sync(container.solve(Dependant(return_four)))
             ) == 5

tests/test_container.py
@@ -50,18 +50,6 @@ def test_enter_scope_subclass(
             assert list(container.scopes) == ["test", "another"]
 
 
-def test_binds_property():
-    container = Container()
-    assert container.binds == {}
-
-    def func() -> None:
-        ...
-
-    dep = Dependant(lambda: None)
-    container.bind(dep, func)
-    assert container.binds == {func: dep}
-
-
 @pytest.mark.parametrize(
     "container_cls",
     [BaseContainer, Container, BaseContainerSubclass, ContainerSubclass],

tests/test_exceptions.py
@@ -49,7 +49,7 @@ def test_dependency_can_catch_exception_single_sync() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     container.execute_sync(container.solve(Dependant(collector)))
     assert rec.caught == {"dep1": True}
 
@@ -61,7 +61,7 @@ async def test_dependency_can_catch_exception_single_async() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     assert rec.caught == {"async_dep1": True}
 
@@ -72,7 +72,7 @@ def test_dependency_can_catch_exception_concurrent_sync() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     container.execute_sync(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -89,7 +89,7 @@ async def test_dependency_can_catch_exception_concurrent_async() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -104,7 +104,7 @@ async def test_dependency_can_catch_exception_concurrent_mixed() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     await container.execute_async(container.solve(Dependant(collector)))
     # one of the dependencies catches and swallows the exception
     # so the other one nevers sees it
@@ -150,7 +150,7 @@ def test_dependency_can_catch_exception_single_sync_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     try:
         container.execute_sync(container.solve(Dependant(collector)))
     except MyException:
@@ -169,7 +169,7 @@ async def test_dependency_can_catch_exception_single_async_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:
@@ -189,7 +189,7 @@ def test_dependency_can_catch_exception_concurrent_sync_reraise() -> None:
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     try:
         container.execute_sync(container.solve(Dependant(collector)))
     except MyException:
@@ -210,7 +210,7 @@ async def test_dependency_can_catch_exception_concurrent_async_reraise() -> None
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:
@@ -231,7 +231,7 @@ async def test_dependency_can_catch_exception_concurrent_mixed_reraise() -> None
 
     container = Container()
     rec = Recorder()
-    container.bind(lambda: rec, Recorder)
+    container.bind(Dependant(lambda: rec), Recorder)
     try:
         await container.execute_async(container.solve(Dependant(collector)))
     except MyException:

tests/test_execute.py
@@ -272,7 +272,7 @@ async def test_concurrency_async(dep1: Any, dep2: Any):
     container = Container()
 
     counter = Counter()
-    container.bind(lambda: counter, Counter)
+    container.bind(Dependant(lambda: counter), Counter)
 
     async def collector(
         a: None = Depends(dep1, share=False, sync_to_thread=True),

tests/test_solving.py
@@ -80,7 +80,7 @@ def test_valid_non_callable_annotation(
     """No type annotations are required if default values are provided"""
 
     container = Container()
-    container.bind(lambda: 1, annotation)
+    container.bind(Dependant(lambda: 1), annotation)
     res = container.execute_sync(container.solve(Dependant(function)))
     assert res == 1
 
@@ -205,9 +205,9 @@ def test_wiring_from_binds() -> None:
             super().__init__(1)
 
     container = Container()
-    # container.bind(CanBeWired, CannotBeWired)
+    # container.bind(Dependant(CanBeWired), CannotBeWired)
     with pytest.raises(WiringError):
         container.solve(Dependant(CannotBeWired))
-    container.bind(CanBeWired, CannotBeWired)
+    container.bind(Dependant(CanBeWired), CannotBeWired)
     c = container.execute_sync(container.solve(Dependant(CannotBeWired)))
     assert isinstance(c, CanBeWired)