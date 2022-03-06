6c95810961d86485ec552c7305883fd571145ab7
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-03-06 16:11:22 -0600

    refactor: simplify di/executors/_concurrency.py

--- di/executors/_concurrency.py
+++ di/executors/_concurrency.py
@@ -1,5 +1,4 @@
 import contextvars
-import functools
 from typing import Any, Awaitable, Callable, TypeVar
 
 import anyio
@@ -7,17 +6,10 @@ import anyio
 T = TypeVar("T")
 
 
-def curry_context(call: Callable[..., T]) -> Callable[..., T]:
-    ctx = contextvars.copy_context()
-
-    def inner(*args: Any, **kwargs: Any) -> T:
-        return ctx.run(functools.partial(call, *args, **kwargs))
-
-    return inner
-
-
 def callable_in_thread_pool(call: Callable[..., T]) -> Callable[..., Awaitable[T]]:
-    def inner(*args: Any, **kwargs: Any) -> Awaitable[T]:
-        return anyio.to_thread.run_sync(curry_context(call), *args, **kwargs)
+    def inner(*args: Any, **kwargs: Any) -> "Awaitable[T]":
+        return anyio.to_thread.run_sync(
+            contextvars.copy_context().run, lambda: call(*args, **kwargs)
+        )  # type: ignore[return-value]
 
     return inner
