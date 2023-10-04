14e330e33db5fb51cac740e20a230344240b6bd9
Max Zhenzhera <maxzhenzhera@gmail.com>
2023-10-04 17:53:42 +0300

    refactor: import available from std typing

--- di/_utils/inspect.py
+++ di/_utils/inspect.py
@@ -1,12 +1,12 @@
 import functools
 import inspect
 import sys
-from typing import Any, Callable, Dict, Mapping, Optional, Union
+from typing import Any, Callable, Dict, Mapping, Optional, Union, get_args, get_origin
 
 if sys.version_info < (3, 9):
-    from typing_extensions import Annotated, get_args, get_origin, get_type_hints
+    from typing_extensions import Annotated, get_type_hints
 else:
-    from typing import Annotated, get_args, get_origin, get_type_hints
+    from typing import Annotated, get_type_hints
 
 from di._utils.types import Some
 
